cfc1dc5b9e50436d142234147b4d45887b39fac4
Yurui Zhang <eraclaymore@hotmail.com>
2013-12-14 15:33:06 -0500

    add get_cells_at method

--- board.rb
+++ board.rb
@@ -1,13 +1,21 @@
 class Board
   def initialize
-    @cell_count = 0
+    @cells = []
   end
 
   def add_cell cell
-    @cell_count += 1
+    @cells.push(cell)
   end
 
   def cell_count
-    @cell_count
+    @cells.size
+  end
+
+  def get_cell_at(x, y)
+    result = nil
+    @cells.each do |c|
+      result = c and break if c.x == x && c.y == y
+    end
+    result
   end
 end
--- board_spec.rb
+++ board_spec.rb
@@ -4,21 +4,32 @@ require_relative 'board'
 require_relative 'cell'
 
 describe Board do
+
+  let(:board) { Board.new }
+
   it "should be able to get the cell at a location" do
-    board = Board.new
     cell = Cell.new 1, 1
 
     board.add_cell cell
+
     expect(board.get_cell_at(1, 1)).to eql(cell)
   end
 
   it "should be able to add a cell" do
-    board = Board.new
     cell = Cell.new 1, 1
 
     board.add_cell cell
 
     expect(board.cell_count).to eq(1)
   end
+
+  it "should be able to count the number of cells around a location" do
+    cell_1 = Cell.new 1,2
+    cell_2 = Cell.new 1,1
+    cell_3 = Cell.new 1,9
+
+    expect(board.number_of_cells_around(0,1)).to eq(2)
+  end
+
 end
 
